2fe5d15401cef13102110e1c37ad7ce66352717b
Jesse Silverberg <jessesilverberg11@gmail.com>
2018-01-18 20:57:21 -0500

    added opencv

--- src/HOG.java
+++ src/HOG.java
@@ -1,4 +1,14 @@
 
-public class HOG {
-
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+ 
+public class HOG
+{
+   public static void main( String[] args )
+   {
+      System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+      Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
+      System.out.println( "mat = " + mat.dump() );
+   }
 }
